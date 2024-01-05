83c4d131c431e83a6a022df63a67d7b470f38eb1
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-05 15:51:53 -0300

    adicionando primeira parte do projeto

---
+++ src/main/java/com/keniaReis/usersdept/UsersDeptApplication.java
@@ -0,0 +1,13 @@
+package com.keniaReis.usersdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersDeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersDeptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keniaReis/usersdept/UsersDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keniaReis.usersdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersDeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
